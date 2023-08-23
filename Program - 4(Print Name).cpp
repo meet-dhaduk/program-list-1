98e285b7c423db9b20e17f5d7803fb3f0c3fb138
meet-dhaduk <141798968+meet-dhaduk@users.noreply.github.com>
2023-08-23 08:39:47 +0530

    Add files via upload

---
+++ Program - 4(Print Name).cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"My Name Is meet dhaduk.";
+	return 0;
+}
---
+++ Program - 5(Swap).cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	cout<<"Enter a first number :: ";
+	cin>>a;
+	cout<<"Enter a second number:: ";
+	cin>>b;
+	c=a;
+	a=b;
+	b=c;
+	cout<<"Value Of a After Swapping Is :: "<<a<<endl;
+	cout<<"Value Of b After Swapping Is :: "<<b<<endl;
+	return 0;
+}
